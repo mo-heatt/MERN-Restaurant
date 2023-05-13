eee6513a22fad57322404b10d389937ebeddee99
Mohit <mohitkumawat12345613@gmail.com>
2023-05-13 11:16:50 +0200

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        {/* desktop */}
+        <div  className=''>
+            <div className=''>
+                <img/>
+            </div>
+        </div>
+
+
+        {/* mobile */}
+
+    </header>
+  )
+}
+
+export default Header

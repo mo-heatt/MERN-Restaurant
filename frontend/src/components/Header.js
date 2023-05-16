81d9459a769679e65873f26556617bf7380461c1
Mohit Kumawat <115658001+mo-heatt@users.noreply.github.com>
2023-05-16 19:26:15 +0000

    headerUpdated

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -20,7 +20,8 @@ const Header = () => {
     toast("Logout successfully");
   };
 
-  const cartItemNumber = useSelector((state)=>state.product.cartItem)
+  const cartItemNumber = useSelector((state)=>state.product.cartItem);
+  
   return (
     <header className="fixed shadow-md w-full h-16 px-2 md:px-4 z-50 bg-white">
       {/* desktop */}

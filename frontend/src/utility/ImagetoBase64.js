252c0af5cdf41c9bc7e9a2c01914b02b8c6dc47b
Mohit Kumawat <115658001+mo-heatt@users.noreply.github.com>
2023-05-25 00:11:13 +0200

    ImagetoBase64.js completed

--- frontend/src/utility/ImagetoBase64.js
+++ frontend/src/utility/ImagetoBase64.js
@@ -1,14 +1,13 @@
-export async function ImagetoBase64(file){
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
+async function ImagetoBase64(file){
+    const reader = new FileReader()  //declaration to read the file
+    reader.readAsDataURL(file)  //read the file as URL
 
-    const data = new Promise((resolve,reject)=>{
-        reader.onload = ()=> resolve(reader.result)
-        reader.onerror = err => reject(err)
+    const data = new Promise((resolve,reject)=>{      //to check if the file is converted or not
+        reader.onload = ()=> resolve(reader.result)   //if done return result
+        reader.onerror = err => reject(err)           //if error reject
     })
-
     return data
 }
 
-export {ImagetoBase64}
+export {ImagetoBase64};
 
